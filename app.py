1c3c399a276e8d5c7a6551201b4dcb3782232fba
iolemenezess <iolemenezess@gmail.com>
2025-10-07 21:43:49 -0300

    quinta alteracao

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Ola Github")
+from flask import Flask, request
+
+app = Flask(__name__)
+
+#endpoint: buscar todos os alunos
+@app.route('buscaralunos', methods=['GET'])
+def buscar_alunos():
+    return "todos alunos listados"
+
+if __name__== "__main__":
+    app.run(debug=True)
+
