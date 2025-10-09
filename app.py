7f1fee1894f27e0d5acb82cae5b8f0231999d2a0
iolemenezess <iolemenezess@gmail.com>
2025-10-09 19:53:48 -0300

    sexta alteracao

--- app.py
+++ app.py
@@ -2,10 +2,44 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
+alunos = [
+    {"id": 1, "nome": "Anerson"},
+    {"id": 2, "nome": "Gabriel"},
+    {"id": 3, "nome": "Brendon"}
+]
+#endpoint: buscar aluno com id
+@app.route('/buscaraluno/<int:id>', methods=['GET'])
+def buscar_alunos(id):
+    return f"aluno de codigo: {id}"
+
 #endpoint: buscar todos os alunos
-@app.route('buscaralunos', methods=['GET'])
-def buscar_alunos():
-    return "todos alunos listados"
+@app.route('/listaralunos', methods=['GET'])
+def listar_alunos():
+
+    return alunos
+
+# endpoint: adicionar aluno query params
+@app.route('/adicionaraluno', methods=['POST']) 
+def adicionar_aluno():
+    id = request.args.get('id')  
+    nome = request.args.get('nome')
+    novo = {"id": id , "nome": nome}
+    novo["id"] = len(alunos)+1
+    alunos.append(novo)
+
+    return f"Aluno Cadastrado: {id} {nome}"
+
+
+#endpoint: adicionar json aluno
+@app.route('/adicionaralunojson', methods=['POST']) 
+def adicionar_aluno_json():
+    novo = request.get_json()
+    novo["id"] = len(alunos)+1
+    alunos.append(novo)
+    return alunos
+
+
 
 if __name__== "__main__":
     app.run(debug=True)
